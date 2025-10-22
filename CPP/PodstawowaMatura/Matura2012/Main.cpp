980ab04ab2a4aa2f94599bfbc6fbc68e83fbc8e3
pestit <pestit@proton.me>
2025-10-22 13:45:19 +0200

    Done matura 2012 advanced

--- CPP/PodstawowaMatura/Matura2012/Main.cpp
+++ CPP/PodstawowaMatura/Matura2012/Main.cpp
@@ -4,6 +4,35 @@
 
 using namespace std;
 
+bool NumIsEven(int num) { // func checks if given num is Even (parzyste)
+    return num % 2 == 0;
+}
+
+int SumDigits(int num) {
+    int sum = 0;
+
+    while (num > 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+
+    return sum;
+}
+
+bool DigitsIncreasingOrder(int num) {
+    int lastNum = num % 10;
+    num /= 10;
+
+    do {
+        if (lastNum <= num % 10) { return false; }
+        lastNum = num % 10;
+        num /= 10;
+    } 
+    while (num > 0);
+
+    return true;    
+}
+
 int stringToInt(string str) { // func that converts string var to int
     int outputNum = 0;
     char tempChar;
@@ -16,27 +45,15 @@ int stringToInt(string str) { // func that converts string var to int
     return outputNum;
 }
 
-bool NumIsEven(int num) { // func checks if given num is Even (parzyste)
-    return num % 2 == 0;
-}
-
-int NumCharSum (int num) {
-    int sum = 0;
-    do {
-        sum += num % 10;
-        num /= 10;
-    } while (num > 0);
-
-    return sum;
-}
-
-
-int main() { // program entry point 
+int main() { 
+    // program entry point 
     fstream inputFile, outputFile;
-
     string tempWord;
-    int tempWordInt;
 
+    int tempNum;
+
+    int lowest = INT_MAX, highest = INT_MIN;
+    int lowestNum, highestNum;
     int counterEven = 0;
 
     inputFile.open("cyfry.txt", ios::in);
@@ -45,16 +62,42 @@ int main() { // program entry point
 
     for (int i = 0; i < 1000; i++) {
         inputFile >> tempWord;
-        tempWordInt = stringToInt(tempWord);
+        tempNum = stringToInt(tempWord);
 
-        if (NumIsEven(stringToInt(tempWord))) { 
+        if (NumIsEven(tempNum)) { 
             counterEven++;
+        }
+
+        if (SumDigits(tempNum) > highest) { highest = SumDigits(tempNum); highestNum = tempNum; }
+        if (SumDigits(tempNum) < lowest) { lowest = SumDigits(tempNum); lowestNum = tempNum; }
+
+    }
+
+    cout << "Zadanie A. Ilość parzystych liczb: " << counterEven << endl << endl;
+    outputFile << "Zadanie A. Ilość parzystych liczb: " << counterEven << endl << endl;
+
+    cout << "Zadanie B. Największa suma cyfr w liczbie: " << highestNum << endl;
+    cout << "Najmniejsza suma cyfr w liczbie: " << lowestNum << endl << endl;
+
+    outputFile << "Zadanie B. Największa suma cyfr w liczbie: " << highestNum << endl;
+    outputFile << "Najmniejsza suma cyfr w liczbie: " << lowestNum << endl << endl;
+
+    inputFile.close();
+
+    inputFile.open("cyfry.txt", ios::in);
+
+    cout << "Zadanie C. Liczby w increasing order: " << endl;
+    outputFile << "Zadanie C. Liczby w increasing order: " << endl;
+
+    for (int i = 0; i < 1000; i++) {
+        inputFile >> tempWord;
+        tempNum = stringToInt(tempWord);
+
+        if (DigitsIncreasingOrder(tempNum)) {
+            cout << tempNum << endl;
+            outputFile << tempNum << endl;
         }
     }
 
-    cout << "1. Ilość parzystych liczb: " << counterEven << endl;
-    outputFile << "1. Ilość parzystych liczb: " << counterEven << endl;
-
-    inputFile.close();
     outputFile.close();
 }
