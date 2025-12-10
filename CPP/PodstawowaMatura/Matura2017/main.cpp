70e91944c281cfd65bbd57dce9b69772e5a6de67
blestafist <pestit@proton.me>
2025-12-10 15:07:34 +0100

    Matura 2017 not done yet

--- CPP/PodstawowaMatura/Matura2017/main.cpp
+++ CPP/PodstawowaMatura/Matura2017/main.cpp
@@ -10,6 +10,9 @@ int FILE_LENGTH = 1000; // lenth of input data array
 
 // =================================================================================
 
+int GCD();
+int SumNum();
+
 // triple struct
 struct ThreeNums {
     int first;
@@ -17,26 +20,68 @@ struct ThreeNums {
     int third;
 
     bool IncreasingOrder() {
-        return first < second && second << third;
+        return first < second && second < third;
     }
+
+	int SumDigitsOfThree() {
+		return SumNum(first) + SumNum(second) + SumNum(third);
+	}
+
+	bool SumEquals35() {
+		return SumDigitsOfThree() == 35;
+	}
 };
 
+int GCD(int x, int y) {
+	int temp;
+	while (y != 0) {
+		temp = x % y;
+		x = y;
+		y = temp;
+	}
+
+	return x;
+}
+
+int SumNum(int num) {
+	int result = 0;
+
+	while (num > 0) {
+		result += num % 10;
+		num /= 10;
+	}
+
+	return result;
+}
 
 int main() {
 	std::ifstream inputFile(INPUT_FILE_NAME);
 	std::ofstream outputFile;
-    short increasingCount = 0;
+    unsigned short increasingCount = 0, sumDigits, sumEquals35 = 0, highestLinesCount = 0;
+	int NWDSum = 0, highest = 0;
+
+	int arraySumsDigits[FILE_LENGTH];
 
 	int tempWord; // string / int (change on need)
 
 	for (int i = 0; i < FILE_LENGTH; i++) {
         ThreeNums nums;
 		inputFile >> nums.first >> nums.second >> nums.third;
+
+		sumDigits = nums.SumDigitsOfThree();
+
         if (nums.IncreasingOrder() == true) { increasingCount++; }
         
+		NWDSum += GCD(nums.first, GCD(nums.second, nums.third)); // 2nd
+		if (nums.SumEquals35()) { sumEquals35++; }
 
+		arraySumsDigits[i] = sumDigits;
 
-		// here place the conditions for nums / strs checking	
+		if (highest < sumDigits) { highest = sumDigits; }
+	}
+
+	for (int i = 0; i < FILE_LENGTH; i++) {
+		
 	}
 
 	inputFile.close();
