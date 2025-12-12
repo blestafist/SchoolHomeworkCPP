cb04fcfe5ccfadcb5e2095389afe9aaae7ef3817
blestafist <pestit@proton.me>
2025-12-12 20:07:11 +0100

    Made fun changing text animation

---
+++ CPP/Fun/changingText.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdint>
+#include <unistd.h>
+
+
+
+const std::string inputWord = "Ja takaja kak vse, on skazal eto vseeem i ja takaja kak vse, glupaja sovsem"; // here place your word 
+const int delayInMicroSeconds = 1; // delay in microseconds (800-1000 recommended)
+
+
+
+char GenerateRandomChar() { // 32 - 126 = 93
+    return rand() % 94 + 32;
+}
+
+std::string GenerateRandomWord() { // generate seed
+    std::string result;
+
+    for (uint8_t i = 0; i < inputWord.size(); i++) {
+        result += GenerateRandomChar();
+    }
+
+    return result;
+}
+
+int main() { // entry point
+    srand(time(NULL));
+
+    std::string inputWordTemp = GenerateRandomWord();
+
+    for (int i = 0; i < inputWord.size(); i++) {
+        while (true) {
+            if (inputWord[i] == GenerateRandomChar()) {
+                inputWordTemp[i] = inputWord[i];
+                std::cout << inputWordTemp << std::endl;
+                break;
+            }
+
+            inputWordTemp[i] = GenerateRandomChar();
+
+            std::cout << inputWordTemp << std::endl;
+            usleep(delayInMicroSeconds);
+        }
+    }
+}
+
