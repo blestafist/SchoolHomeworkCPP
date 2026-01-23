69c1f9288d1649cc4999c032df2ce8c07fb4760c
ufw <ufw-public@proton.me>
2026-01-23 23:09:15 +0100

    Fixed PermutNum

--- CPP/Tasks/PermutationNum.cpp
+++ CPP/Tasks/PermutationNum.cpp
@@ -2,7 +2,7 @@
 Problem: Write out all the nums <1; 10000> which each permutation is Prime
 
 1. Check if number is prime, then convert it into std::string
-2. Check if number has an even number (so 1 of permutation'll be with 0 at the end → num is non-prime or if)
+2. Check if number has an even number or 5(so 1 of permutation'll be with 0 at the end → num is non-prime or if)
 3. If not, check all the permutations IsPrime
 4, Print the result of counter
 */
@@ -22,8 +22,8 @@ bool IsPrime(unsigned short num) { // max is 10000, none of permutation > 32768
     return true;
 }
 
-bool HasNoEven(const std::string& str) {
-    const char notEvenNums[5] = {'0', '2', '4', '6', '8'};
+bool IsValid(const std::string& str) {
+    const char notEvenNums[6] = {'0', '2', '4', '5', '6', '8'};
 
     for (char c : str) {
         for (char badC : notEvenNums) {
@@ -43,7 +43,7 @@ int main() { // entry point
             if (i < 10) { counter++; continue; } // next operations on num >= 10
             std::string sNum = std::to_string(i);
 
-            if (HasNoEven(sNum)) {
+            if (IsValid(sNum)) {
                 std::sort(sNum.begin(), sNum.end());
 
                 bool allPrime = true;
