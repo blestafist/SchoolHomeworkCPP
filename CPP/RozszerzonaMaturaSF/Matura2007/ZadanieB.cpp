59b0743b494a00b03f08a4ee5feff97fc2ad5db7
ufw <claudaiku@pm.me>
2026-01-31 20:51:56 +0100

    done Matura 2007 (fully

--- CPP/RozszerzonaMaturaSF/Matura2007/ZadanieB.cpp
+++ CPP/RozszerzonaMaturaSF/Matura2007/ZadanieB.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iostream>
 
 /*
@@ -27,10 +28,30 @@ bool IsPrime(unsigned short num) { // most efficient IsPrime (3-5x faster than D
     return true;
 }
 
-bool SumDigitsPrime(unsigned short num) {
-    
+bool SumDigitsPrime(unsigned short num) { // 2
+    short result = 0;
+
+    while (num > 0) {
+        result += num % 10;
+        num /= 10;
+    }
+
+    return IsPrime(result);
 }
 
 int main() {
+    unsigned short counter = 0, tempNum;
+    long sumResult = 0;
 
+    for (unsigned short i = 100; i < 10000; i++) {
+        if (SumDigitsPrime(i)) { counter++; }
+    }
+
+    std::ifstream inputFile ("2.txt");
+
+    while (inputFile >> tempNum) {
+        tempNum += sumResult;
+    }
+
+    std::cout << counter << "\n" << IsPrime(sumResult) << "\n";
 }
