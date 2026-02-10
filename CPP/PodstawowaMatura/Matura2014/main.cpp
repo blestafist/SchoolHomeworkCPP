73b48bc9c3a99656a090eb44ebc9fd6fb1f821ab
ufw <claudaiku@pm.me>
2026-02-10 22:25:22 +0100

    Made code review for Matura 2014 PSF and corrected code

--- CPP/PodstawowaMatura/Matura2014/main.cpp
+++ CPP/PodstawowaMatura/Matura2014/main.cpp
@@ -1,111 +1,75 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
+#include <utility>
 
 // ================================ CONFIGURATION ===================================
 
-std::string INPUT_FILE_NAME = "PARY_LICZB.TXT"; // input file name here
-std::string OUTPUT_FILE_NAME = "ZADANIE5.TXT"; // output file name here
-
-int FILE_LENGTH = 1000; // lenth of input data array (num of pairs)
+const std::string INPUT_FILE_NAME = "PARY_LICZB.TXT"; // input file name here
+const std::string OUTPUT_FILE_NAME = "ZADANIE5.TXT"; // output file name here
 
 // =================================================================================
 
-// minimal wrapper for program (remove comment)
 
-// two value struct 
-struct Pair {
-    public:
-        int x;
-        int y;
-
-        Pair(int _x, int _y) {
-            x = _x;
-            y = _y;
-        }
-};
-
-// end of structs
-
-// addictional funcs
-
-
-bool MultipleNumOfTwo(int* x, int* y) {
-    if (*x == 0 || *y == 0) { return false; } 
-
-    if (*x > *y) { return *x % *y == 0; }
-    return *y % *x == 0;
+bool IsMultiplier(short num1, short num2) {
+    if (num1 > num2) { return num1 % num2 == 0; }
+    return num2 % num1 == 0;
 }
 
-bool HighestBothDivider(int x, int y) {
-    if (x == 0 || y == 0) { return false; }
-    while (x != y) {
-        if (x > y) {
-            x -= y;
-        }
-
-        else {
-            y -= x;
-        }
+bool GPD1(short num1, short num2) {
+    while (num2) { // same as num2 != 0
+        num1 %= num2;
+		std::swap(num1, num2);
     }
 
-    return x == 1;
+	return num1 == 1;
 }
 
-int SumNum(int x) {
-    int result = 0;
-    
-    while (x > 0) {
-        result += x % 10;
-        x /= 10;
-    }
+unsigned char SumDigits(short num) {
+	unsigned char result = 0;
 
-    return result;
+	while (num > 0) {
+		result += num % 10;
+		num /= 10;
+	}
+
+	return result;
 }
 
-bool SumNumEquals(int* x, int* y) {
-    return SumNum(*x) == SumNum(*y);
-}
-
-// program entry point 
 
 int main() {
-	std::ifstream inputFile(INPUT_FILE_NAME);
-	std::ofstream outputFile; // not opening it yet
-
-	int firstNum, secondNum;
-    std::vector<Pair> pairs; // vector, that'll be containing pairs of numbers
+	std::ifstream inputFile (INPUT_FILE_NAME);
+	if (!inputFile.is_open()) { std::cerr << "Error while opening input file!"; return -1; }
 
 
+	std::ofstream outputFile;
 
-	for (int i = 0; i < FILE_LENGTH; i++) {
-		inputFile >> firstNum >> secondNum;
+	short tempNum1, tempNum2; 
+    short multiplierCounter = 0, gpd1Counter = 0, sumDigitsEqCounter = 0;
 
-        pairs.push_back({firstNum, secondNum});	
+	// int tempNum;
+
+	while (inputFile >> tempNum1 >> tempNum2) {
+		if (IsMultiplier(tempNum1, tempNum2)) { multiplierCounter++; }
+		if (GPD1(tempNum1, tempNum2)) { gpd1Counter++; }
+
+		if (SumDigits(tempNum1) == SumDigits(tempNum2)) { sumDigitsEqCounter++; }
+
+		// here place the conditions for nums / strs checking	
 	}
 
 	inputFile.close();
 	
 	outputFile.open(OUTPUT_FILE_NAME);
-    int countA = 0, countB = 0, countC = 0;
+	if (!outputFile.is_open()) { std::cerr << "Error while opening output file!"; return -1; }
 
-	// write answers
-    for (int i = 0; i < pairs.size(); i++) {
-        if (MultipleNumOfTwo(&pairs[i].x, &pairs[i].y)) { countA++; }
-        if (HighestBothDivider(pairs[i].x, pairs[i].y)) { countB++; }
-        if (SumNumEquals(&pairs[i].x, &pairs[i].y)) { countC++; }
-    }
+	std::cout << "a) Liczba par, w ktorych jedna liczba jest wielokrotnoscia drugiej: " << multiplierCounter << "\n\n";
+    std::cout << "b) Liczba par wzglednie pierwszych (NWD = 1): " << gpd1Counter << "\n\n";
+    std::cout << "c) Liczba par o tej samej sumie cyfr: " << sumDigitsEqCounter << "\n\n";
 
-    std::cout << "Zadanie A: " << countA << std:: endl;
-    std::cout << "Zadanie B: " << countB << std:: endl;
-    std::cout << "Zadanie C: " << countC << std:: endl;
-
-    outputFile << "Zadanie A: " << countA << std:: endl;
-    outputFile << "Zadanie B: " << countB << std:: endl;
-    outputFile << "Zadanie C: " << countC << std:: endl;
-
+	outputFile << "a) Liczba par, w ktorych jedna liczba jest wielokrotnoscia drugiej: " << multiplierCounter << "\n\n";
+    outputFile << "b) Liczba par wzglednie pierwszych (NWD = 1): " << gpd1Counter << "\n\n";
+    outputFile << "c) Liczba par o tej samej sumie cyfr: " << sumDigitsEqCounter << "\n\n";
 	
 	outputFile.close();
-
 	return 0;
 }
