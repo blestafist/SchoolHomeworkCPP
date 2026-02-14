99b44df1758dc4b641c98263abc48b53e6cb21bc
Qsiry <qsiry.defolt@gmail.com>
2026-02-14 16:45:09 +0100

    fixed Minimal-Wrapper-Matura

--- CPP/Wrappers/Minimal-Wrapper-Matura.cpp
+++ CPP/Wrappers/Minimal-Wrapper-Matura.cpp
@@ -4,15 +4,14 @@
 const std::string INPUT_FILE_NAME = "";
 const std::string OUTPUT_FILE_NAME = "";
 
-int main ()
+int main()
 {
-    std::ifstream inputFile (INPUT_FILE_NAME);
-    if (!inputFile) { std::cerr << "Error while opening inputFile\n"; return -1;}
+    std::ifstream inputFile(INPUT_FILE_NAME);
+    if (!inputFile) { std::cerr << "Error while opening inputFile\n"; return -1; }
 
-    std::ofstream outputFile (OUTPUT_FILE_NAME);
-    if (!outputFile) { std::cerr << "Error while opening outputFile\n"; return -1;}
+    std::ofstream outputFile(OUTPUT_FILE_NAME);
+    if (!outputFile) { std::cerr << "Error while opening outputFile\n"; return -1; }
 
-    
     std::string tempString;
 
     while (inputFile >> tempString)
