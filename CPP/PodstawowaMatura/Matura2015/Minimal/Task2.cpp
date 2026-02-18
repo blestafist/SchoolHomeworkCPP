129c80cf4e56773407fef95f84967dd79bfcb4db
ufw <claudaiku@pm.me>
2026-02-19 00:36:34 +0100

    Added a so minimal version (without comments)

--- CPP/PodstawowaMatura/Matura2015/Minimal/Task2.cpp
+++ CPP/PodstawowaMatura/Matura2015/Minimal/Task2.cpp
@@ -4,14 +4,9 @@
 #include <unordered_map>
 #include <vector>
 
-// ======================== CONFIGURATION ========================
-
 const std::string INPUT_FILE_NAME = "slowa.txt";
 const std::string NEW_INPUT_FILE_NAME = "nowe.txt";
-
 const std::string OUTPUT_FILE_NAME = "wynik5.txt";
-
-// ===============================================================
 
 int main() {
     std::ifstream inputFileWords (INPUT_FILE_NAME), inputFileNew (NEW_INPUT_FILE_NAME);
@@ -26,32 +21,19 @@ int main() {
     std::unordered_map<std::string, int> words, mirrorWords;
     std::vector<std::string> seq;
 
-
     while (inputFileNew >> tempWord) {
         seq.push_back(tempWord);
         words[tempWord] = 0;
         mirrorWords[rev(tempWord)] = 0;
     }
 
-    inputFileNew.close(); // end of work with inputFileNew
-
-    // now working with words
     while (inputFileWords >> tempWord) {
-        if (words.count(tempWord) != 0) {
-            words[tempWord]++;
-        }
-
-        if (mirrorWords.count(tempWord)) {
-            mirrorWords[tempWord]++;
-        }
+        if (words.count(tempWord) != 0) { words[tempWord]++; }
+        if (mirrorWords.count(tempWord)) { mirrorWords[tempWord]++; }
     }
-
-    // now displaying answers
 
     for (const std::string& str : seq) {
         std::cout << str << " → " << words[str] << " → " << mirrorWords[rev(str)] << "\n";
         outputFile << str << " → " << words[str] << " → " << mirrorWords[rev(str)] << "\n";
     }
-
-    return 0;
 }
