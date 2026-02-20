12bab991f072c9637018dc967ef54f697afa744b
ufw <claudaiku@pm.me>
2026-02-20 21:22:54 +0100

    Changed logic of ParseFile and added FindMinMax func

--- CPP/RozszerzonaMaturaNF/Matura2017/full.cpp
+++ CPP/RozszerzonaMaturaNF/Matura2017/full.cpp
@@ -15,16 +15,18 @@ const int vectorY[] = { 1, -1, 0, 0 };
 
 // ==================================================================================
 
+
 void ParseFile(unsigned char (&arr)[DIMENSIONS_X][DIMENSIONS_Y]) {
     std::ifstream inputFile (INPUT_FILE_NAME);
     if (!inputFile) { std::cerr << "Error while opening output file!"; throw -1; }
 
     short current;
 
-    for (int x = 0; x < DIMENSIONS_X; x++) {
-        for (int y = 0; y < DIMENSIONS_Y; y++) {
-            inputFile >> current;
-            arr[x][y] = current;
+    for (int y = 0; y < DIMENSIONS_Y; y++) {
+        for (int x = 0; x < DIMENSIONS_X; x++) {
+            if (inputFile >> current) {
+                arr[x][y] = (unsigned char)current;
+            }
         }
     }
 }
@@ -41,7 +43,8 @@ void PrintArr(const unsigned char (&arr)[DIMENSIONS_X][DIMENSIONS_Y]) {
     }
 }
 
-bool ContrastNeightboor(const unsigned char (&arr)[DIMENSIONS_X][DIMENSIONS_Y]) {
+
+int ContrastNeightboor(const unsigned char (&arr)[DIMENSIONS_X][DIMENSIONS_Y]) {
     // first define the conditions for checking
     int counter = 0;
 
@@ -54,7 +57,7 @@ bool ContrastNeightboor(const unsigned char (&arr)[DIMENSIONS_X][DIMENSIONS_Y]) 
                 int ny = y + vectorY[i];
 
                 if (nx >= 0 && nx < DIMENSIONS_X && ny >= 0 && ny < DIMENSIONS_Y) {
-                    if (std::abs(arr[x][y] - arr[nx][ny]) > 128) { isContrast = true; break; }
+                    if (std::abs((int)arr[x][y] - (int)arr[nx][ny]) > 128) { isContrast = true; break; }
                 }
             }
 
@@ -65,9 +68,27 @@ bool ContrastNeightboor(const unsigned char (&arr)[DIMENSIONS_X][DIMENSIONS_Y]) 
     return counter;
 }
 
+
+void FindMinMax(unsigned char (&arr)[DIMENSIONS_X][DIMENSIONS_Y], unsigned char& min, unsigned char& max) {
+    for (int y = 0; y < DIMENSIONS_Y; y++) {
+        for (int x = 0; x < DIMENSIONS_X; x++) {
+            if (arr[x][y] < min) { min = arr[x][y]; }
+            if (arr[x][y] > max) { max = arr[x][y]; }
+        }
+    }
+}
+
+
+int NumLinesToRemove(unsigned char (&arr)[DIMENSIONS_X][DIMENSIONS_Y]) {
+}
+
+
 int main() {
     unsigned char mainArr[DIMENSIONS_X][DIMENSIONS_Y];
+    unsigned char min = 255, max = 0; // opposite values
+
     ParseFile(mainArr);
+    PrintArr(mainArr);
     
-    int counter
+    std::cout << ContrastNeightboor(mainArr);
 }
