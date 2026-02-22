28a4c347ba8cf445e5167a00b99414b053e5f5d8
ufw <claudaiku@pm.me>
2026-02-22 21:22:48 +0100

    Started Matura 2016

---
+++ CPP/RozszerzonaMaturaSF/Matura2016/ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+
+// =========================== CONFIGURATION ===========================
+
+const std::string INPUT_FILE = "gra.txt";
+const std::string OUTPUT_FILE = "wynik4.txt";
+
+const int DIMENSIONS_X = 12;
+const int DIMENSIONS_Y = 20;
+
+// =====================================================================
