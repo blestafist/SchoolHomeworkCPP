ddfde363a079a2b7d63431e26189ad95e7398a7b
ufw <claudaiku@pm.me>
2026-02-25 21:39:03 +0100

    Fixed Task2.cpp

--- CPP/RozszerzonaMaturaNF/Matura2021/Task2.cpp
+++ CPP/RozszerzonaMaturaNF/Matura2021/Task2.cpp
@@ -1,22 +1,14 @@
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <unordered_map>
 
 // =========================== CONFIGURATION ===========================
 
 const std::string INPUT_FILE_NAME = "galerie_przyklad.txt";
-const std::string OUTPUT_FILE_NAME = "wynik4_X.txt";
+const std::string OUTPUT_FILE_NAME = "wynik4_2.txt";
 
-const int PAIR_COUNT = 0;
+const int PAIR_COUNT = 70;
 
 // =====================================================================
-
-struct Pair {
-    public:
-        int x;
-        int y;
-};
 
 
 int main() {
@@ -26,19 +18,22 @@ int main() {
     std::ofstream outputFile (OUTPUT_FILE_NAME);
     if (!outputFile) { std::cerr << "Error while opening output file!"; return -1; }
 
-    std::string town;
-    Pair tempPair, hashPair; // hashPair -- first → area | second → num of lcl
-    std::unordered_map<std::string, Pair> map; // first → name of town, second is hash pair
+    std::string countryCode, town;
 
-    while (inputFile >> town >> town) {
-        hashPair.x = 0; hashPair.y = 0;
+    while (inputFile >> countryCode >> town) {
+        int totalArea = 0, totalCount = 0;
         for (int i = 0; i < PAIR_COUNT; i++) {
-            inputFile >> tempPair.x >> tempPair.y;
-            if (tempPair.x == 0) { inputFile.ignore(10000, '\n'); break; }
-            hashPair.x += tempPair.x * tempPair.y;
-            hashPair.y++;
+            int x, y;
+            inputFile >> x >> y;
+
+            if (x > 0 && y > 0) {
+                totalArea += x * y;
+                ++totalCount;
+            }
         }
-        map[town] = hashPair;
+
+        std::cout << town << ' ' << totalArea << ' ' << totalCount << '\n';
+        outputFile << town << ' ' << totalArea << ' ' << totalCount << '\n';
     }
 
     return 0;
