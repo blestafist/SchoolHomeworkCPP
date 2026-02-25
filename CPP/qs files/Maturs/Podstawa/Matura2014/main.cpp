4bd0f37416d1e9a4111c891ccb5e1e15481e8a51
ufw <claudaiku@pm.me>
2026-02-25 21:59:45 +0100

    Code review && minor fixes in QS Matura 2014 PSF

--- CPP/qs files/Maturs/Podstawa/Matura2014/main.cpp
+++ CPP/qs files/Maturs/Podstawa/Matura2014/main.cpp
@@ -22,13 +22,13 @@ bool IsSumOfDigitsSame(short num1, short num2)
 {
     short sum1 = 0, sum2 = 0;
 
-    while(num1 > 0)
+    while (num1 > 0)
     {
         sum1 += num1 % 10;
         num1 /= 10;
     }
 
-    while(num2 > 0)
+    while (num2 > 0)
     {
         sum2 += num2 % 10;
         num2 /= 10;
@@ -45,6 +45,8 @@ int main()
     std::ofstream outputFile(OUTPUT_FILE_NAME);
     if (!outputFile) { std::cerr << "Error while openig outputFile\n"; return -1; }
 
+    auto Print = [&] (auto&&... args) { (std::cout << ... << args) << "\n"; (outputFile << ... << args) << "\n"; };
+
     short tempNum1, tempNum2, 
     counterDivisible = 0, 
     counterNOD1 = 0, 
@@ -52,18 +54,14 @@ int main()
 
     while (inputFile >> tempNum1 >> tempNum2)
     {
-        if(tempNum1 % tempNum2 == 0 || tempNum2 % tempNum1 == 0) { counterDivisible ++; }
+        if (tempNum1 % tempNum2 == 0 || tempNum2 % tempNum1 == 0) { counterDivisible ++; }
 
-        if(GCD(tempNum1, tempNum2) == 1) { counterNOD1++; }
+        if (GCD(tempNum1, tempNum2) == 1) { counterNOD1++; }
 
-        if(IsSumOfDigitsSame(tempNum1, tempNum2)) { counterSameSum++; }
+        if (IsSumOfDigitsSame(tempNum1, tempNum2)) { counterSameSum++; }
     }
 
-    std::cout <<"ZadanieA\n" << counterDivisible <<"\n";
-    std::cout <<"ZadanieB\n" << counterNOD1 <<"\n";
-    std::cout <<"ZadanieC\n" << counterSameSum <<"\n";
-
-    outputFile <<"ZadanieA\n" << counterDivisible <<"\n";
-    outputFile <<"ZadanieB\n" << counterNOD1 <<"\n";
-    outputFile <<"ZadanieC\n" << counterSameSum <<"\n";
+    Print("ZadanieA: ", counterDivisible);
+    Print("ZadanieB: ", counterNOD1);
+    Print("ZadanieC: ", counterSameSum);
 }
