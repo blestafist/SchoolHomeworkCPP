48e51295dd185caa805916973ec253cc78bbd379
ufw <claudaiku@pm.me>
2026-02-25 20:16:55 +0100

    base.cpp

---
+++ CPP/RozszerzonaMaturaNF/Matura2021/base.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+
+// =========================== CONFIGURATION ===========================
+
+const std::string INPUT_FILE_NAME = "galerie_przyklad.txt";
+const std::string OUTPUT_FILE_NAME = "wynik4_X.txt";
+
+// =====================================================================
+
+int main() {
+    std::ifstream inputFile (INPUT_FILE_NAME);
+    if (!inputFile) { std::cerr << "Error while opening input file!"; return -1; }
+
+    std::ofstream outputFile (OUTPUT_FILE_NAME);
+    if (!outputFile) { std::cerr << "Error while opening output file!"; return -1; }
+
+    while (inputFile >> XX) {
+
+    }
+
+    return 0;
+}
