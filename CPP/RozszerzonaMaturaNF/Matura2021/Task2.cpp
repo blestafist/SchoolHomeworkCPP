57557d82bb6937d5c13eaf27efca2d925672f4ab
ufw <claudaiku@pm.me>
2026-02-25 21:49:34 +0100

    Fixed Task2.cpp and changed origin file

--- CPP/RozszerzonaMaturaNF/Matura2021/Task2.cpp
+++ CPP/RozszerzonaMaturaNF/Matura2021/Task2.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <fstream>
+#include <limits.h>
 
 // =========================== CONFIGURATION ===========================
 
-const std::string INPUT_FILE_NAME = "galerie_przyklad.txt";
+const std::string INPUT_FILE_NAME = "galerie.txt";
 const std::string OUTPUT_FILE_NAME = "wynik4_2.txt";
 
 const int PAIR_COUNT = 70;
@@ -18,7 +19,10 @@ int main() {
     std::ofstream outputFile (OUTPUT_FILE_NAME);
     if (!outputFile) { std::cerr << "Error while opening output file!"; return -1; }
 
-    std::string countryCode, town;
+    auto Print = [&] (auto&&... args) { (std::cout << ... << args) << "\n"; (outputFile << ... << args) << "\n"; };
+
+    std::string countryCode, town, maxAreaLabel, minAreaLabel;
+    int maxArea = 0, minArea = INT_MAX;
 
     while (inputFile >> countryCode >> town) {
         int totalArea = 0, totalCount = 0;
@@ -32,9 +36,14 @@ int main() {
             }
         }
 
-        std::cout << town << ' ' << totalArea << ' ' << totalCount << '\n';
-        outputFile << town << ' ' << totalArea << ' ' << totalCount << '\n';
+        Print(town, ' ', totalArea, ' ', totalCount);
+
+        if (maxArea < totalArea) { maxArea = totalArea; maxAreaLabel = town; }
+        if (minArea > totalArea) { minArea = totalArea; minAreaLabel = town; }
     }
+
+    Print("\nNajwiększe: ", maxAreaLabel, ' ', maxArea);
+    Print("Najmniejsze: ", minAreaLabel, ' ', minArea);
 
     return 0;
 }
