db82612d5639f217bc459d69af0209998a42e8c6
ufw <claudaiku@pm.me>
2026-03-02 20:15:15 +0100

    Second ver of TitleToNumber, now works correctly

--- CPP/Tasks/LeetCode/2.cpp
+++ CPP/Tasks/LeetCode/2.cpp
@@ -5,8 +5,9 @@ int titleToNumber(string columnTitle) {
     int result = 0;
 
     for (char c : columnTitle) {
-        result = (result * 26) + (c - 65);
+        int digit = c - 'A' + 1;
+        result = (result * 26) + digit;
     }
 
-    return result + 1;
+    return result;
 }
