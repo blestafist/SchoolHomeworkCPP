24ca225a2a16794a6a03d38d30b10b6a8a0d6f16
ufw <claudaiku@pm.me>
2026-03-03 20:06:35 +0100

    Started ComputeArea task on LeetCode

--- CPP/Tasks/LeetCode/2.cpp
+++ CPP/Tasks/LeetCode/2.cpp
@@ -1,5 +1,7 @@
+#include <cstdlib>
 #include <iostream>
 using namespace std;
+
 
 int titleToNumber(string columnTitle) {
     int result = 0;
@@ -10,4 +12,13 @@ int titleToNumber(string columnTitle) {
     }
 
     return result;
+}
+
+
+int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+    // ! A is a first rectangle
+    // ! B is a second rectangle
+
+    int rectangleCoverArea;
+    rectangleCoverArea = abs(bx1 - ax2) * abs(by1 - ay2);
 }
