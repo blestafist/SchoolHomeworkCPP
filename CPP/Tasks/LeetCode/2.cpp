96de6983f21c30eacf2121d15d7d1d703f082050
ufw <claudaiku@pm.me>
2026-03-03 20:25:34 +0100

    Done MySqrt func from LeetCode (Task 69)

--- CPP/Tasks/LeetCode/2.cpp
+++ CPP/Tasks/LeetCode/2.cpp
@@ -22,3 +22,23 @@ int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, i
     int rectangleCoverArea;
     rectangleCoverArea = abs(bx1 - ax2) * abs(by1 - ay2);
 }
+
+int mySqrt(int x) {
+    if (x < 2) { return x; }
+
+    int left = 2, right = x / 2;
+    int mid, res = 1;
+
+    while (left <= right) {
+        mid = left + (right - left) / 2;
+
+        if (mid <= x / mid) //x eq to mid * mid <= x
+        {
+            res = mid; // remebmering a possible result
+            left = mid + 1;
+        }
+        else { right = mid - 1; }
+    }
+
+    return res;
+}
