76022b9794ab1b7f9100ebafd5c7a912a2455c4c
ufw <claudaiku@pm.me>
2026-03-08 19:29:49 +0100

    Fixes for multiple file input in Matura 2005 C

--- CPP/RozszerzonaMaturaSF/Matura2005/TaskB.cpp
+++ CPP/RozszerzonaMaturaSF/Matura2005/TaskB.cpp
@@ -3,22 +3,24 @@
 
 // =========================== CONFIGURATION ===========================
 
-const std::string INPUT_FILE_NAME = "dane5-3.txt";
+const std::string INPUT_FILE_NAMES[] = { "dane5-1.txt", "dane5-2.txt", "dane5-3.txt" };
 
 // =====================================================================
 
 int main() {
-    std::ifstream inputFile (INPUT_FILE_NAME);
-    if (!inputFile) { std::cerr << "Error while opening input file!"; return -1; }
+    for (const std::string& fileName : INPUT_FILE_NAMES) {
+        std::ifstream inputFile (fileName);
+        if (!inputFile) { std::cerr << "Error while opening input file!"; return -1; }
 
-    int localSum = 0,
-    maxSum = 0,
-    currInt;
+        int localSum = 0,
+        maxSum = 0,
+        currInt;
 
-    while (inputFile >> currInt) {
-        localSum = std::max(localSum + currInt, currInt);
-        if (localSum > maxSum) { maxSum = localSum; }
+        while (inputFile >> currInt) {
+            localSum = std::max(localSum + currInt, currInt);
+            if (localSum > maxSum) { maxSum = localSum; }
+        }
+
+        std::cout << "Dla pliku " << fileName << " → " << maxSum << "\n";
     }
-
-    std::cout << maxSum;
 }
--- CPP/RozszerzonaMaturaSF/Matura2005/TaskC.cpp
+++ CPP/RozszerzonaMaturaSF/Matura2005/TaskC.cpp
@@ -4,27 +4,29 @@
 
 // =========================== CONFIGURATION ===========================
 
-const std::string INPUT_FILE_NAME = "dane5-1.txt";
+const std::string INPUT_FILE_NAMES[] = { "dane5-1.txt", "dane5-2.txt", "dane5-3.txt" };
 
 // =====================================================================
 
 int main() {
-    std::ifstream inputFile (INPUT_FILE_NAME);
-    if (!inputFile) { std::cerr << "Error while opening input file!"; return -1; }
+    for (const std::string& fileName : INPUT_FILE_NAMES) {
+        std::ifstream inputFile (fileName);
+        if (!inputFile) { std::cerr << "Error while opening input file!"; return -1; }
 
-    std::unordered_map<int, int> map; // element : occ
+        std::unordered_map<int, int> map; // element : occ
 
-    int currInt,
-    maxOccValue = 0,
-    maxOccKey = 0;
+        int currInt,
+        maxOccValue = 0,
+        maxOccKey = 0;
 
-    while (inputFile >> currInt) {
-        map[currInt]++;
+        while (inputFile >> currInt) {
+            map[currInt]++;
+        }
+
+        for (const auto& [key, value] : map) {
+            if (maxOccValue < value) { maxOccValue = value; maxOccKey = key; }
+        }
+
+        std::cout << "Dla pliku " << fileName << " → " << maxOccKey << "\n";
     }
-
-    for (const auto& [key, value] : map) {
-        if (maxOccValue < value) { maxOccValue = value; maxOccKey = key; }
-    }
-
-    std::cout << maxOccKey;
 }
