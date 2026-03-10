cd971e922c0a0a1ca4c425a551062fad1575eb24
ufw <claudaiku@pm.me>
2026-03-10 18:20:07 +0100

    Fixed IsPrime func to more effective solution

--- CPP/RozszerzonaMaturaSF/Matura2007/ZadanieA.cpp
+++ CPP/RozszerzonaMaturaSF/Matura2007/ZadanieA.cpp
@@ -12,11 +12,11 @@
 */
 
 bool IsPrime(int num) { // 1
-    if (num == 2) { return true; }
-    if (num < 2 || num % 2 == 0) { return false; }
+    if (num == 2 || num == 3) { return true; }
+    if (num % 2 == 0 || num % 3 == 0 || num < 2) { return false; }
 
-    for (int i = 3; i * i <= num; i += 2) {
-        if (num % i == 0) { return false; }
+    for (int i = 5; i * i <= num; i += 6) {
+        if (num % i == 0 || num % (i + 2) == 0) { return false; }
     }
 
     return true;
