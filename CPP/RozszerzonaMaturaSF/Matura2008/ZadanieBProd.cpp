cf279c93abe026fca0091d01b952a7f4d7ab2e72
ufw <claudaiku@pm.me>
2026-03-14 15:49:39 +0100

    Done all logic

--- CPP/RozszerzonaMaturaSF/Matura2008/ZadanieBProd.cpp
+++ CPP/RozszerzonaMaturaSF/Matura2008/ZadanieBProd.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <stdexcept>
 
 // ============================================ CONFIGURATION ============================================
 
@@ -35,5 +36,34 @@ std::string ComposeW2(const std::string& origin) {
 
 
 int main() {
+    std::ifstream inputFile (INPUT_FILE_NAME);
+    if (!inputFile) { std::cerr << "Error while opening input file!"; return -1; }
+
+    std::ofstream passFile (PASS_FILE_NAME), answersFile (ANSWERS_FILE_NAME);
+    if (!passFile || !answersFile) { std::cerr << "Error while opening output files!"; return -1; }
+
+    auto Print = [&] (auto&&... args) { (std::cout << ... << args) << "\n"; (answersFile << ... << args) << "\n"; };
+
+    std::string tempWord, 
+    longestStr = "",
+    shortestStr = "",
+    contains12Chars = "";
+
+    int longest = -1,
+    shortest = -1,
+    sumLength;
+
+    while (inputFile >> tempWord) {
+        std::string composed = ComposeW2(tempWord);
+        int len = composed.size();
+
+        std::cout << composed << "\n";
+        passFile << composed << "\n";
+
+        if (len == 12) { contains12Chars += composed + "\n"; }
+        if (longest == -1 || len > longest) { longest = len; longestStr = composed; }
+        if (shortest == -1 || len < shortest) { shortest = len; shortestStr = composed; }
+    }
+
 
 }
