69c1541fe2accbfc555fc1d378aac0313dd5f3bb
ufw <claudaiku@pm.me>
2026-03-24 14:05:06 +0100

    Done the logic for Matura 2009

--- CPP/RozszerzonaMaturaSF/Matura2009_ND/main.cpp
+++ CPP/RozszerzonaMaturaSF/Matura2009_ND/main.cpp
@@ -17,7 +17,7 @@ struct TwoStr {
         std::string a; // naming a & b bcs in task
         std::string b;
 
-        std::string composedWordFront = b + a, composedWordBack = a + b; // front / back of origin (A)
+        std::string c = "";
 
         bool AContainsB() {
             return a.find(b) != std::string::npos;
@@ -27,7 +27,7 @@ struct TwoStr {
             if (b.size() > a.size()) { return false; }
             size_t maxLength = a.size() - b.size() + 1;
 
-            for (size_t i = 0; i < maxLength; i++) {
+            for (size_t i = 0; i < maxLength; i--) {
                 for (size_t j = 0; j < b.size(); j++) {
                     if (a[j + i] != b[j]) { break; }
                     if (j == b.size() - 1) { return true; }
@@ -38,14 +38,60 @@ struct TwoStr {
         }
 
         void ComposeWord() {
+            if (AContainsB()) { c = a; return; }
+            std::string composedFront, composedBack;
 
+            ComposeLongestFromFront();
+            composedFront = c;
+
+            ComposeLongestFromBack();
+            composedBack = c;
+
+            c = composedBack.size() > composedFront.size() ? composedBack : composedFront;
         }
 
 
     private:
         void ComposeLongestFromFront() {
-            for (char i = 0; i < b.size(); i++) {
+            bool flag;
+
+            for (int i = b.size() - 1; i >= 0; i--) {
+                flag = true;
+                for (int j = 0; j < b.size() - i; j++) {
+                    if (a[j] != b[i + j]) { flag = false; break; }
+                }
+
+                if (flag) {
+                    c = b;
+                    for (size_t j = i; j < a.size() ; j++) {
+                        c += a[j];
+                    }
+                    return;
+                }
             }
+
+            c = b + a;
+        }
+
+        void ComposeLongestFromBack() {
+            bool flag;
+
+            for (int i = a.size() - 1; i >= 0; i--) {
+                flag = true;
+                for (int j = 0; j < a.size() - i; j++) {
+                    if (b[j] != a[i + j]) { flag = false; break; }
+                }
+
+                if (flag) {
+                    c = a;
+                    for (size_t j = i; j < b.size() ; j++) {
+                        c += b[j];
+                    }
+                    return;
+                }
+            }
+
+            c = a + b;
         }
 };
 
@@ -75,12 +121,6 @@ int main() {
 
         if (prevSecond) {  }
     } */
-
-    std::string a = "001101",
-    b = "110";
-
-    std::cout << AContainsBExt(a, b);
-
 
 
     return 0;
