81f5e014aaa377850e99a80dd0d521e8f1adbe4d
ufw <claudaiku@pm.me>
2026-03-31 15:45:30 +0200

    Fixed C in Matura 2009 C RSF

--- CPP/RozszerzonaMaturaSF/Matura2009_ND/ZadanieC.cpp
+++ CPP/RozszerzonaMaturaSF/Matura2009_ND/ZadanieC.cpp
@@ -1,43 +1,42 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 
 // =========================== CONFIGURATION ===========================
 
-const std::string INPUT_FILE = "dane.txt";
-const std::string OUTPUT_FILE = "zad_5.txt";
+const std::string INPUT_FILE_NAME = "dane.txt";
+const std::string OUTPUT_FILE_NAME = "zad_5.txt";
 
 // =====================================================================
 
-bool Match(const std::string& s1, const std::string& s2, int offset) {
-    for (int i = 0; offset + i < s1.size() && i < s2.size(); ++i) {
-        if (s1[offset + i] != s2[i]) return false;
+bool Match(const std::string& a, const std::string& b, int offset) {
+    for (int i = 0; i + offset < a.size() && i < b.size(); i++) {
+        if (a[i + offset] != b[i]) { return false; }
     }
+
     return true;
 }
 
-
 int main() {
-    std::ifstream inputFile(INPUT_FILE);
-    std::ofstream outputFile(OUTPUT_FILE, std::ios::app);
+    std::ifstream inputFile(INPUT_FILE_NAME);
+    std::ofstream outputFile(OUTPUT_FILE_NAME, std::ios::app);
     
     std::string a, b;
     int concatenationOnlyCount = 0;
     
     while (inputFile >> a >> b) {
         bool hasAnyOverlap = false;
-
-        for (int i = 0; i < a.size(); i++) {
+        for (int i = 0; i < a.size() && !hasAnyOverlap; i++) {
             if (Match(a, b, i)) { hasAnyOverlap = true; }
         }
 
-        for (int i = 1; i < b.size(); i++) {
+        for (int i = 1; i < b.size() && !hasAnyOverlap; i++) {
             if (Match(b, a, i)) { hasAnyOverlap = true; }
         }
 
-        if (!hasAnyOverlap) { concatenationOnlyCount++;  }
+        if (!hasAnyOverlap) { concatenationOnlyCount++; }
     }
     
+    std::cout << "c) " << concatenationOnlyCount << "\n";
     outputFile << "c) " << concatenationOnlyCount << "\n";
     
     return 0;
