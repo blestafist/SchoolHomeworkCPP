760a3ae5d8e61f94c462a49eb742212b77b4d4f3
ufw <claudaiku@pm.me>
2026-04-02 21:15:47 +0200

    Remade Matura 2016 to optimise memory usage (8x lower)!

--- CPP/RozszerzonaMaturaSF/Matura2016/Task1.cpp
+++ CPP/RozszerzonaMaturaSF/Matura2016/Task1.cpp
@@ -15,43 +15,45 @@ int velocityY[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
 
 // =====================================================================
 
-void ParseFile(char (&arr)[DIMENSIONS_X][DIMENSIONS_Y]) {
+void ParseFile(bool (&arr)[DIMENSIONS_X][DIMENSIONS_Y]) {
     std::ifstream inputFile (INPUT_FILE_NAME);
     if (!inputFile) { throw std::runtime_error("Error while opening input file!"); }
+    char currC;
 
     for (int y = 0; y < DIMENSIONS_Y; y++) {
         for (int x = 0; x < DIMENSIONS_X; x++) {
-            inputFile >> arr[x][y];
+            inputFile >> currC;
+            currC == 'X' ? arr[x][y] = true : arr[x][y] = false; 
         }
     }
 }
 
 
-int SumOfNeightbors(const char (&arr)[DIMENSIONS_X][DIMENSIONS_Y], int indexX, int indexY) {
+int SumOfNeightbors(const bool (&arr)[DIMENSIONS_X][DIMENSIONS_Y], int indexX, int indexY) {
     int aliveNeightbors = 0;
 
     for (int i = 0; i < 8; i++) { // counting all alive neightbors
         int nx = (indexX + velocityX[i] + DIMENSIONS_X) % DIMENSIONS_X;
         int ny = (indexY + velocityY[i] + DIMENSIONS_Y) % DIMENSIONS_Y;
 
-        if (arr[nx][ny] == 'X') { aliveNeightbors++; }
+        if (arr[nx][ny]) { aliveNeightbors++; }
     }
 
     return aliveNeightbors;
 }
 
 
-void SimulateNextGen(char (&arr)[DIMENSIONS_X][DIMENSIONS_Y]) {
-    char helperArr[DIMENSIONS_X][DIMENSIONS_Y] {};
+void SimulateNextGen(bool (&arr)[DIMENSIONS_X][DIMENSIONS_Y]) {
+    bool helperArr[DIMENSIONS_X][DIMENSIONS_Y] {};
 
     for (int y = 0; y < DIMENSIONS_Y; y++) {
         for (int x = 0; x < DIMENSIONS_X; x++) {
             int aliveNeightbors = SumOfNeightbors(arr, x, y);
-            helperArr[x][y] = '.';
+            helperArr[x][y] = false;
 
             // now calculating
-            if ((aliveNeightbors == 3 && arr[x][y] == '.') || ((aliveNeightbors == 2 || aliveNeightbors == 3) && arr[x][y] == 'X')) {
-                helperArr[x][y] = 'X';
+            if ((aliveNeightbors == 3 && !arr[x][y]) || ((aliveNeightbors == 2 || aliveNeightbors == 3) && arr[x][y])) {
+                helperArr[x][y] = true;
             }
         }
     }
@@ -67,7 +69,7 @@ void SimulateNextGen(char (&arr)[DIMENSIONS_X][DIMENSIONS_Y]) {
 
 
 int main() {
-    char mainArr[DIMENSIONS_X][DIMENSIONS_Y];
+    bool mainArr[DIMENSIONS_X][DIMENSIONS_Y];
 
     std::ofstream outputFile (OUTPUT_FILE_NAME);
     if (!outputFile) { std::cerr << "Error while opening output file!"; return -1; }
