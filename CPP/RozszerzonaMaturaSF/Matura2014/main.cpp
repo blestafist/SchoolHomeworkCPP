84cca591fa00ddd579d51a676f8f7bf8218a9258
ufw <claudaiku@pm.me>
2026-04-07 18:08:07 +0200

    Major changes in main.cpp (Matura 2014 C RSF). Fixed: - Using unordered_map instead of map - Optimised SumASCIIPrime func (using foreach) - No vector → using string with \n for increasingStrings - Other minor changes

--- CPP/RozszerzonaMaturaSF/Matura2014/main.cpp
+++ CPP/RozszerzonaMaturaSF/Matura2014/main.cpp
@@ -1,73 +1,67 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include <map>
-
+#include <unordered_map>
 
 // =========================== CONFIGURATION ===========================
 
-const std::string INPUT_FILE = "NAPIS.TXT";
-const std::string OUTPUT_FILE = "ZADANIE5.TXT";
+const std::string INPUT_FILE_NAME = "NAPIS.TXT";
+const std::string OUTPUT_FILE_NAME = "ZADANIE5.TXT";
 
 // =====================================================================
 
-bool IsPrime(unsigned short num) {
-    if (num < 2) { return false; }
+bool IsPrime(int num) {
     if (num == 2 || num == 3) { return true; }
-    if (num % 2 == 0 || num % 3 == 0) { return false; }
+    if (num % 2 == 0 || num % 3 == 0 || num < 2) { return false; }
 
-    for (unsigned short i = 5; i * i <= num; i += 6) {
+    for (int i = 5; i * i <= num; i += 6) {
         if (num % i == 0 || num % (i + 2) == 0) { return false; }
     }
 
     return true;
 }
 
-bool SumCodesPrime(const std::string& str) {
+bool SumASCIIPrime(const std::string& str) {
     int result = 0;
-    for (char c : str) { result += c; }
+
+    for (char c : str) {
+        result += c;
+    }
+
     return IsPrime(result);
 }
 
-bool IncreasingNum(const std::string& str) {
-    for (char i = 0; i < str.size() - 1; i++) {
+bool IncreasingString(const std::string& str) {
+    for (int i = 0; i < str.size() - 1; i++) {
         if (str[i] >= str[i + 1]) { return false; }
     }
+
     return true;
 }
 
-
 int main() {
-    std::ifstream inputFile (INPUT_FILE);
-    std::ofstream outputFile (OUTPUT_FILE);
+    std::ifstream inputFile (INPUT_FILE_NAME);
+    std::ofstream outputFile (OUTPUT_FILE_NAME);
+    if (!inputFile) { std::cerr << "Error while opening input file!"; return -1; }
 
-    std::string tempWord;
-    std::vector<std::string> incerasingWords;
-    std::map<std::string, char> ctr;
+    std::string tempWord, increasingStrings;
+    size_t primeStringsCount = 0;
 
-    short primeWordsCounter;
+    std::unordered_map<std::string, int> wordsOcc; // word : count
+    auto Print = [&] (auto&&... args) { (std::cout << ... << args) << "\n"; (outputFile << ... << args) << "\n"; };
 
     while (inputFile >> tempWord) {
-        if (SumCodesPrime(tempWord)) { primeWordsCounter++; }
+        if (SumASCIIPrime(tempWord)) { primeStringsCount++; }
+        if (IncreasingString(tempWord)) { increasingStrings += tempWord + "\n"; }
 
-        if(IncreasingNum(tempWord)) { incerasingWords.push_back(tempWord); }
-
-        ctr[tempWord]++;
+        wordsOcc[tempWord]++;
     }
 
-    std::cout << "Ilość napisów pierwszych: " << primeWordsCounter;
-    outputFile << "Ilość napisów pierwszych: " << primeWordsCounter;
+    Print("Ilość napisów pierwszych: ", primeStringsCount);
+    Print("\nNapisy rosnące: \n", increasingStrings);
+    Print("\nNapisy, występujące więcej niż 1 raz: ");
 
-    std::cout << "\n\nNapisy rosnące: \n";
-    outputFile << "\n\nNapisy rosnące: \n";
-
-    for (std::string word : incerasingWords) { std::cout << word << "\n"; outputFile << word << "\n"; }
-
-    std::cout << "\nNapisy, które występują więcej niż 1 raz: \n";
-    outputFile << "\nNapisy, które występują więcej niż 1 raz: \n";
-
-    for (const auto& [str, count] : ctr) {
-        if (count > 1) { std::cout << str << "\n"; outputFile << str << "\n"; }
+    for (const auto& [key, value] : wordsOcc) {
+        if (value > 1) { Print(key); }
     }
 
     return 0;
