042fb12204ae54d49c3102073f45b828cd913b5c
ufw <claudaiku@pm.me>
2026-04-08 23:10:11 +0200

    In ZadanieD.cpp from Matura C 2009 RSF added self-written ContainsSubstring method. Also regenerated file zad_5.txt with correct answers

--- CPP/RozszerzonaMaturaSF/Matura2009/ZadanieD.cpp
+++ CPP/RozszerzonaMaturaSF/Matura2009/ZadanieD.cpp
@@ -12,6 +12,22 @@ bool ContainsSubstring(const std::string& a, const std::string& b) {
     return a.find(b) != std::string::npos;
 }
 
+bool ContainsSubstringFull(const std::string& str1, const std::string& str2) {
+    if (str1.size() < str2.size()) { return false; }
+    size_t maxLen = str1.size() - str2.size() + 1;
+
+    for (size_t shift = 0; shift < maxLen; shift++) {
+        bool match = true;
+        for (size_t i = 0; i < str2.size(); i++) {
+            if (str1[shift + i] != str2[i]) { match = false; break; }
+        }
+
+        if (match) { return true; }
+    }
+
+    return false; 
+}
+
 std::string MergeWithOverlap(const std::string& first, const std::string& second) {
     int maxOverlap = 0;
     
