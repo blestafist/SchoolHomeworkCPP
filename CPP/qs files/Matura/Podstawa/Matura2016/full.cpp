b9e8cacb4e75211f0f23e924ab5a3ee8f366dfa6
Qsiry <qsiry.defolt@gmail.com>
2026-04-29 23:55:15 +0200

    Refactor C2016: optimize IsPrime function with 6k +/- 1 rule (skip multiples of 2 and 3)

--- CPP/qs files/Matura/Podstawa/Matura2016/full.cpp
+++ CPP/qs files/Matura/Podstawa/Matura2016/full.cpp
@@ -17,9 +17,11 @@ using Pair = std::pair<int, int>;
 
 
 bool IsPrime(int num) {
-    if (num < 2) { return false; }
-    for (int i = 2; i * i <= num; ++i) {
-        if (num % i == 0) { return false; }
+    if (num == 2 || num == 3) { return true; }
+    if (num % 2 == 0 || num % 3 == 0 || num < 2) { return false; }
+
+    for (int i = 5; i * i <= num; i += 6) {
+        if (num % i == 0 || num % (i + 2) == 0) { return false; }
     }
     return true;
 }
